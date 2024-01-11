32fd57d75dc9858746aacd67492cfd825914ea81
tushar-dev001 <tushar789imran@gmail.com>
2024-01-12 04:36:01 +0600

    some changes

--- index.js
+++ index.js
@@ -23,15 +23,9 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
 
     const pictureCollection = client.db('taskWithFrontend').collection('picture')
-
-    // app.post('/picture', async(req, res)=>{
-    //     const picture = req.body;
-    //     const result = await pictureCollection.insertOne(picture)
-    //     res.send(result)
-    // })
 
     app.get('/photo',async(req, res)=>{
       const result = await pictureCollection.find().toArray()
@@ -44,13 +38,11 @@ async function run() {
       res.send(result)
     })
 
-
-
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log(
-      "Pinged your deployment. You successfully connected to MongoDB!"
-    );
+    // await client.db("admin").command({ ping: 1 });
+    // console.log(
+    //   "Pinged your deployment. You successfully connected to MongoDB!"
+    // );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
